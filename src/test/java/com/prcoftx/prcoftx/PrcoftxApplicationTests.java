eca759cb1a342c42bd64750034fdb035802ee79b
Kenneth Howell <howell.kenneth.a@gmail.com>
2020-06-20 09:17:01 -0500

    initial commit for project structure

---
+++ src/test/java/com/prcoftx/prcoftx/PrcoftxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prcoftx.prcoftx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrcoftxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
